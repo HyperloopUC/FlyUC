62891983ce377f1745539cb6b5a6923aaee7428d
Dival Banerjee <bandiv2011@gmail.com>
2019-03-28 18:42:08 -0400

    button class normal state is complete
    Im so happy!

--- GCS/FlyUCGCS/src/com/FlyUC/GUI/UIComponents/cButton.java
+++ GCS/FlyUCGCS/src/com/FlyUC/GUI/UIComponents/cButton.java
@@ -10,8 +10,8 @@ public class cButton extends JComponent {
 
     private static final boolean IS_HIGH_QUALITY = true;
 
-    private int height;
-    private int width;
+    private int buttonHeight;
+    private int buttonWidth;
     private int horizontalMargin;
     private int verticalMargin;
     private int numberOfRoundedCorners;
@@ -22,35 +22,40 @@ public class cButton extends JComponent {
     private Color primaryColor;
     private Color foreGroundColor;
     private Font buttonFont;
+    private int buttonShadow;
 
     public cButton(){
         this.setBackground(new Color(0,0,0,0));
-        this.setHeight(50);
-        this.setWidth(150);
+        this.setButtonHeight(50);
+        this.setButtonWidth(250);
         this.verticalMargin = 10;
         this.horizontalMargin = 10;
         this.fontSize = 20;
         this.buttonFont = new Font("Helvetica", Font.PLAIN, this.fontSize);
         this.caption = "Text";
+        this.buttonShadow  = 3;
     }
 
     public cButton(String caption){
         this.setBackground(new Color(0,0,0,0));
-        this.setHeight(50);
-        this.setWidth(250);
+        this.setButtonHeight(50);
+        this.setButtonWidth(250);
         this.verticalMargin = 10;
         this.horizontalMargin = 10;
         this.fontSize = 20;
         this.buttonFont = new Font("Helvetica", Font.PLAIN, this.fontSize);
         this.caption = caption;
+        this.isClicked = false;
+        this.isHovering = false;
+        this.buttonShadow = 3;
     }
 
-    public void setWidth(int x){
-        this.width = x;
+    public void setButtonWidth(int x){
+        this.buttonWidth = x;
     }
 
-    public void setHeight(int y){
-        this.height = y;
+    public void setButtonHeight(int y){
+        this.buttonHeight = y;
     }
 
     public void setCaption(String text){
@@ -59,12 +64,12 @@ public class cButton extends JComponent {
 
     public void setHorizontalMargin(int margin){
         this.horizontalMargin = margin;
-        this.setWidth(this.getWidth()+(2*margin));
+        //this.setButtonWidth(this.getWidth()+(2*margin));
     }
 
     public void setVerticalMargin(int margin){
         this.verticalMargin = margin;
-        this.setHeight(this.getHeight()+(2*margin));
+        //this.setHeight(this.getHeight()+(2*margin));
     }
 
     public void setfontSize(int size) { this.fontSize = size; }
@@ -75,9 +80,9 @@ public class cButton extends JComponent {
 
     public void setFont(String font){ this.buttonFont = new Font(this.buttonFont.getFontName(), Font.PLAIN, this.fontSize); }
 
-    public int getButtonHeight(){ return this.height; }
+    public int getButtonHeight(){ return this.buttonHeight; }
 
-    public int getButtonWidth(){ return this.width; }
+    public int getButtonWidth(){ return this.buttonWidth; }
 
     public void setIsHovering(){
         if(this.isHovering){
@@ -107,15 +112,36 @@ public class cButton extends JComponent {
             }
         }else{
             //Not hovering, normal state
-            int  startX= ((getWidth()/2 - this.getButtonWidth()/2)+this.horizontalMargin + this.getButtonHeight()/2);
-            int finalX = ((getWidth()-this.getButtonWidth()/2)-this.horizontalMargin - this.getButtonHeight()/2);
-            int startY = ((getHeight()/2 - this.getButtonHeight() + this.verticalMargin));
-            int finalY = ((getWidth()/2 - this.getButtonHeight()) - this.verticalMargin);
-            g2.fillRect(startX,startY,finalX,finalY);
+            //int  startX= ((getWidth()/2 - this.getButtonWidth()/2)+this.horizontalMargin + this.getButtonHeight()/2);
+            int startX = (this.getWidth()/2)-(this.getButtonWidth()/2) + this.getButtonHeight()/2;
+            //int finalX = ((getWidth()-this.getButtonWidth()/2));//-this.horizontalMargin - this.getButtonHeight()/2);
+           // int finalX = (this.getButtonWidth() - (this.getButtonHeight()/2)) - this.horizontalMargin;
+            int finalX = this.getButtonWidth()-this.getButtonHeight()-this.horizontalMargin;
+            int startY = (((this.getHeight()/2 - this.getButtonHeight()/2) + this.verticalMargin));
+            System.out.println(startX);
+            System.out.println("Final X:");
+            //System.out.println(finalX);
+            g2.setColor(new Color(this.primaryColor.getRed()-25,this.primaryColor.getGreen()-25,this.primaryColor.getBlue()-25));
+            g2.fillOval((int)(this.getWidth()/2)-(this.getButtonWidth()/2) + this.horizontalMargin+this.buttonShadow,startY+this.buttonShadow,this.getButtonHeight(),this.getButtonHeight());
+            g2.fillOval((int)((startX + finalX))-(this.getButtonHeight()/2)+this.horizontalMargin+this.buttonShadow,startY + this.buttonShadow,this.getButtonHeight(),this.getButtonHeight());
+            // int finalY = ((getHeight()/2) + this.getButtonHeight() - this.verticalMargin);
+            // int finalY = startY + this.getButtonHeight() - this.verticalMargin;
+            g2.fillRect(startX+this.horizontalMargin+this.buttonShadow,startY + this.buttonShadow,finalX,this.getButtonHeight());
+
+            g2.setColor(this.primaryColor);
+            g2.fillOval((int)(this.getWidth()/2)-(this.getButtonWidth()/2) + this.horizontalMargin,startY,this.getButtonHeight(),this.getButtonHeight());
+            g2.fillOval((int)((startX + finalX))-(this.getButtonHeight()/2)+this.horizontalMargin,startY,this.getButtonHeight(),this.getButtonHeight());
+           // int finalY = ((getHeight()/2) + this.getButtonHeight() - this.verticalMargin);
+           // int finalY = startY + this.getButtonHeight() - this.verticalMargin;
+            g2.fillRect(startX+this.horizontalMargin,startY,finalX,this.getButtonHeight());
+
+
             //g2.fillOval(this.verticalMargin/2 + this.horizontalMargin);
           //FIXME: Button does not center   g2.fillRect((this.getParent().getWidth()/2)+(this.height/2)-this.getWidth(),verticalMargin,(this.width-(this.getHeight()/2)-horizontalMargin),this.height-verticalMargin);
         }
         g2.setColor(this.foreGroundColor);
-        g2.drawString(this.caption, (this.getWidth() - getFontMetrics(buttonFont).stringWidth(this.caption))/2, ((this.getHeight() - this.fontSize)/2)-verticalMargin);
+        g2.drawString(this.caption, (this.getWidth() - getFontMetrics(buttonFont).stringWidth(this.caption))/2, (int)(((this.getHeight() + this.fontSize)/2)+this.verticalMargin/1.5));
+       // g2.setColor(Color.CYAN);
+       // g2.drawOval((this.getWidth()/2)-5,(this.getHeight()/2)+5,10,10);
     }
 }
